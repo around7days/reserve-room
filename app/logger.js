adf8cb12880a2846a1b30054629ef1fadb4b223c
7days <around.7days@live.jp>
2020-12-04 16:22:27 +0900

    commit

--- app/logger.js
+++ app/logger.js
@@ -1,14 +1,5 @@
 const log4js = require('log4js');
-<<<<<<< HEAD
-const logger = log4js.getLogger('app操作D');
-=======
-const logger = log4js.getLogger('app操作C');
->>>>>>> branch 'main' of https://github.com/around7days/reserve-room.git
+const logger = log4js.getLogger('app');
 logger.level = 'debug';
 
 module.exports = logger;
-操作B
-test
-test
-
-操作A

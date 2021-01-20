3a7a644dcfc561145bf80a572a63b9c1bb893610
7days <around.7days@live.jp>
2021-01-20 12:34:26 +0900

    Merge branch 'main' of https://github.com/around7days/reserve-room into main

--- app/router.js
+++ app/router.js
@@ -5,16 +5,16 @@ const validator = require('./validator');
 const logger = require('./logger');
 
 /** 会議室一覧を取得するAPI */
-router.get('/rooms', controller.doGetRooms);
+router.get('/api/rooms', controller.doGetRooms);
 /** 予約情報（1件）を取得するAPI */
-router.get('/reserve/:id', validator.doGetReserveId, controller.doGetReserveId);
+router.get('/api/reserve/:id', validator.doGetReserveId, controller.doGetReserveId);
 /** 予約情報一覧を取得するAPI */
-router.get('/reserves/search', validator.doGetReservesSearch, controller.doGetReservesSearch);
+router.get('/api/reserves/search', validator.doGetReservesSearch, controller.doGetReservesSearch);
 /** 予約情報を登録するAPI */
-router.post('/reserve', validator.doPostReserve, controller.doPostReserve);
+router.post('/api/reserve', validator.doPostReserve, controller.doPostReserve);
 /** 予約情報を変更するAPI */
-router.put('/reserve', validator.doPutReserve, controller.doPutReserve);
+router.put('/api/reserve', validator.doPutReserve, controller.doPutReserve);
 /** 予約情報を削除するAPI */
-router.delete('/reserve', validator.doDeleteReserve, controller.doDeleteReserve);
+router.delete('/api/reserve', validator.doDeleteReserve, controller.doDeleteReserve);
 
 module.exports = router;
